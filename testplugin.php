73361c583aea7ea8346f1886fd19173bc3e88a09
rod773 <rodrigoarcadio1@gmail.com>
2024-03-23 16:48:23 +0100

    update

--- testplugin.php
+++ testplugin.php
@@ -6,3 +6,16 @@ Plugin Name: Test Plugin
 Author: Rodrigo
 
 */
+
+
+function Activar(){}
+
+
+
+function Desactivar(){}
+
+
+register_activation_hook(__FILE__,Activar);
+
+
+register_activation_hook(__FILE__,Desactivar);
