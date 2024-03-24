72f8a7ce18f5b6d7e01ee429194dee210d65cb07
rod773 <rodrigoarcadio1@gmail.com>
2024-03-24 10:54:35 +0100

    update

--- testplugin.php
+++ testplugin.php
@@ -2,8 +2,9 @@
 
 /*
 Plugin Name: Test Plugin
-
 Author: Rodrigo
+Description: Este es un plugin de prueba
+Version: 0.0.1
 
 */
 
@@ -21,4 +22,25 @@ function Desactivar(){
  register_activation_hook(__FILE__,'Activar');
 
 
- register_activation_hook(__FILE__,'Desactivar');
+ register_activation_hook(__FILE__,'Desactivar'); 
+
+ add_action( 'admin_nenu', 'CrearMenu' );
+
+
+function CrearMenu(){
+    add_menu_page(
+        'Super Encuestas', // Titulo de la página
+        'Super Encuestas Menu', // Titulo del menu
+        'manage_options', // Capability
+        'sp_menu', // Slug
+        'MostrarContenido', // función del contenido
+        plugin_dir_url( __FILE__ ).'admin/img/icon.png',
+        1
+    );
+   
+}
+
+
+function MostrarContenido(){
+    echo "<h1>Contenido de la Página</h1>";
+}
